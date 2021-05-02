1cde726d8e7aad24cc74a00749d68c14f5df5399
NekoPlus <john369478@gmail.com>
2021-05-02 21:01:26 +0800

    header

--- grade_system/src/storge/GradeSystems.java
+++ grade_system/src/storge/GradeSystems.java
@@ -1,4 +1,14 @@
 package storge;
+/** ***************************************************
+class GradeSystems  aGradeSystem儲存a tree of anEntry objects with key ID and value aGrade
+
+containID (ID)  //看aGradeSystem有否含此ID
+getName(ID)   //取得ID的name
+GradeSystems () //建構 aGradeSystem
+showGrade (ID)
+showRank (ID)
+updateWeights ()   
+********************************************************/
 
 public class GradeSystems {
 
