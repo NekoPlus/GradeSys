9ee0c27a923b7bf6ee9b867a0531857b02f717d1
eric28042649 <eric10041004@gmail.com>
2021-05-02 21:29:54 +0800

    grades

--- grade_system/src/storge/Grades.java
+++ grade_system/src/storge/Grades.java
@@ -8,5 +8,15 @@ Grades () { }
 ************************************************************************/
 
 public class Grades {
+	public String name;
+	public int lab1, lab2, lab3;
+	public int midtermExam, finalExam;
+	public int totalGrade;
 	
+	public int calculateTotalGrade(float[] weights) {
+		int aTotalGrade = Math.round(lab1 * weights[0] + lab2 * weights[1]
+				+ lab3 * weights[2] + midtermExam * weights[3] + finalExam
+				* weights[4]);
+		return aTotalGrade;
+	}
 }
