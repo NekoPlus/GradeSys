e97769c4c8241184e11fce76ea090fc5dc3a2a57
NekoPlus <john369478@gmail.com>
2021-05-09 22:21:38 +0800

    TESTBUILDy

--- grade_system/src/test/Test_checkID.java
+++ grade_system/src/test/Test_checkID.java
@@ -1,14 +1,37 @@
 package test;
+/*****************************************************************************-
+class TestCheckID  
+test case 1: ID 96z001044 (2打成z)
+test case 2: ID 96200104 (遺漏最後的4)
+test case 3: ID 962001044 (正確，凌宗廷的ID)
+**************************************************************************** */
 
 import static org.junit.Assert.*;
+import SourceCodeFiles.*;
 
 import org.junit.Test;
 
 public class Test_checkID {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test (expected = IDExceptions1.class)
+	public void testCheckID1() throws IDExceptions1, IDExceptions2{
+		//aUI calls checkID(96z001044);
+		boolean b=checkID("96z001044");
+		assertEquals (b, false);
 	}
+	
+	@Test (expected = IDExceptions2.class)
+	public void testCheckID2() throws IDExceptions1, IDExceptions2{
+		//aUI calls checkID(96200104);
+		boolean b=checkID("96200104");
+		assertEquals (b, false);
+	}
+	
+	@Test
+	public void testCheckID3() throws IDExceptions1, IDExceptions2{
+		boolean b=checkID("962001044");
+		assertEquals (b, true);
+	}
+
 
 }
--- grade_system/src/test/Test_showRank.java
+++ grade_system/src/test/Test_showRank.java
@@ -1,14 +1,22 @@
 package test;
+/****************************************************************************
+class TestShowRank
+aGradeSystem ¶È§t­â©v§Ê¤Î§õ«Â§Ê
+
+test case 1: assert ­â©v§Ê rank equals 2
+test case 2: assert §õ«Â§Ê rank equals 1
+***************************************************************************/
 
 import static org.junit.Assert.*;
+import SourceCodeFiles.*;
 
 import org.junit.Test;
 
 public class Test_showRank {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGradeSystrems1() {
+		assertEquals (2, showRank("962001044"));
 	}
 
 }
