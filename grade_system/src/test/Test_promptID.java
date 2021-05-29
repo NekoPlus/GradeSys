0fb410437c2a3c5883691f5b3c2a17a772b37958
eric19991004 <eric10041004@gmail.com>
2021-05-29 18:23:32 +0800

    new branch4

--- grade_system/src/test/Test_promptID.java
+++ grade_system/src/test/Test_promptID.java
@@ -13,11 +13,7 @@ import SourceCodeFiles.*;
 public class Test_promptID {
 	
 	@Test
-<<<<<<< HEAD
 	public void testPromptID1() throws IDExceptions1, IDExceptions2, IDExceptions3 {
-=======
-	public void test() throws IDExceptions1, IDExceptions2, IDExceptions3 {
->>>>>>> 449d1d834729c659344d7b580fb8336bcb68f73b
 		/*用 setIn 設定console input 為inContent，使用者輸入的字串Q */
 		ByteArrayInputStream inContent = new ByteArrayInputStream("Q".getBytes());
 		System.setIn(inContent);
@@ -28,28 +24,18 @@ public class Test_promptID {
 
 		/* 執行promptID returns使用者輸入的字串*/
 		UI aUI = new UI();
-<<<<<<< HEAD
 		String userInput = aUI.promptID();  
 		/*預期promptID會顯示"輸入ID或 Q (結束使用)？" */
 		assertEquals("輸入ID或 Q (結束使用)?", outContent.toString());
 		/*預期user input 是 Q */ 
-		assertEquals ("Q", userInput);
-=======
-		String userInput = aUI.promptID();
-		
-		/*預期promptID會顯示"輸入ID或 Q (結束使用)？" */
-		assertEquals("輸入ID或 Q (結束使用)?", outContent.toString());
-		/*預期user input 是 Q */ 
 		assertEquals ("Q", userInput.toString());
-		
 
->>>>>>> 449d1d834729c659344d7b580fb8336bcb68f73b
 
 	}
 	@Test
 	public void testPromptID2() throws IDExceptions1, IDExceptions2, IDExceptions3 {
 		/*用 setIn 設定console input 為inContent，使用者輸入的字串Q */
-		ByteArrayInputStream inContent = new ByteArrayInputStream("Q".getBytes());
+		ByteArrayInputStream inContent = new ByteArrayInputStream("962001044".getBytes());
 		System.setIn(inContent);
 
 		/*用 setOut設定console output 為outContent */
@@ -62,7 +48,7 @@ public class Test_promptID {
 		/*預期promptID會顯示"輸入ID或 Q (結束使用)？" */
 		assertEquals("輸入ID或 Q (結束使用)?", outContent.toString());
 		/*預期user input 是 Q */ 
-		assertEquals ("Q", userInput);
+		assertEquals ("962001044", userInput.toString());
 
 	}
 }
