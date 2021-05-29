f7e7e0089311a3019f70fd5102045eb3a59ba591
NekoPlus <john369478@gmail.com>
2021-05-29 16:43:38 +0800

    EXPceeeeeeeeeeeeeetion

--- grade_system/src/test/Test_promptID.java
+++ grade_system/src/test/Test_promptID.java
@@ -2,13 +2,23 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+
 import org.junit.Test;
+
+import SourceCodeFiles.IDExceptions1;
+import SourceCodeFiles.IDExceptions2;
+import SourceCodeFiles.IDExceptions3;
+import SourceCodeFiles.UI;
 
 public class Test_promptID {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws IDExceptions1, IDExceptions2, IDExceptions3 {
+		ByteArrayInputStream in = new ByteArrayInputStream("Q".getBytes());
+		System.setIn(in);
+		UI c = new UI();
+		
 	}
 
 }
