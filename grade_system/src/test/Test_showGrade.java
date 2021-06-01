be61c51a62eb75e42b4e294dabd88786824d0e64
eric19991004 <eric10041004@gmail.com>
2021-06-01 22:06:11 +0800

    new branch6

--- grade_system/src/test/Test_showGrade.java
+++ grade_system/src/test/Test_showGrade.java
@@ -13,21 +13,16 @@ import SourceCodeFiles.GradeSystems;
 public class Test_showGrade {
 
 	@Test
-	public void test() {
-		/*用 setIn 設定console input 為inContent，使用者輸入的字串Q */
-		ByteArrayInputStream inContent = new ByteArrayInputStream("962001044".getBytes());
-		System.setIn(inContent);
-
+	public void Test_showGrade1() {
 		/*用 setOut設定console output 為outContent */
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut (new PrintStream (outContent));
 
 		/* 執行promptID returns使用者輸入的字串*/
 		GradeSystems aGradeSystems = new GradeSystems();
-		boolean isShowGrade = aGradeSystems.showGrade(inContent.toString());  
-		/*預期promptID會顯示"輸入ID或 Q (結束使用)？" */
-		assertEquals(true, isShowGrade);
-		//assertEquals("凌宗廷 87 86 98 88 87\n", outContent.toString());
+
+		assertEquals(true, aGradeSystems.showGrade("962001044"));
+		assertEquals("凌宗廷 87 86 98 88 87\n", outContent.toString());
 	}
 
 }
